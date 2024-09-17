c9392cf28a785e409239c51d661afde69af7b895
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-17 21:07:49 +1000

    chnagesadd

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,5 +1,16 @@
 # Your Solution
 #!/bin/bash
-set -e
-docker push saipriyamvada21/book_catalog:latest
-docker push saipriyamvada21/inventory_management:latest
+# set -e
+# docker push saipriyamvada21/book_catalog:latest
+# docker push saipriyamvada21/inventory_management:latest
+
+#!/bin/bash
+
+# Set image name
+IMAGE_NAME="saipriyamvada21/book_catalog"
+
+# Log in to Docker Hub
+docker login -u saipriyamvada21 -p Saibabapriya@21
+
+# Push the image
+docker push $IMAGE_NAME
