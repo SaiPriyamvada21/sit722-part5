5eae051a969519fe4aa4fcee9b6e89fe40209fd4
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-17 20:53:21 +1000

    third commit

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,4 +1,5 @@
 # Your Solution
 #!/bin/bash
+set -e 
 docker build -t saipriyamvada21/book_catalog:latest ./book_catalog
 docker build -t saipriyamvada21/inventory_management:latest ./inventory_management
