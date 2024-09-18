c25dd9eef77539966e8b10b13b7e4411946edb2e
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-19 00:09:30 +1000

    new values

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,6 +1,8 @@
-# Publishes a Docker image.
-#./scripts/push-image.sh
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$REGISTRY_UN"
+: "$REGISTRY_PW"
 
-#echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push ${CONTAINER_REGISTRY}/book_catalog:${VERSION}
-docker push ${CONTAINER_REGISTRY}/inventory_management:${VERSION}
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+docker push $CONTAINER_REGISTRY/book_catalog:latest
+docker push $CONTAINER_REGISTRY/inventory_management:latest
