c22bc49990eb66dffc7ebad3f5b85baeb4944dc0
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-18 15:33:57 +1000

    new

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -10,4 +10,14 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  provider "azurerm" {
+  features {}
+  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 }
--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -9,3 +9,18 @@ variable location {
 variable kubernetes_version {    
   default = "1.30.0"
 }
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
