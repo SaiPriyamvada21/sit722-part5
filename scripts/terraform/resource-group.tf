7641e8944f6f2838634fcc24bd319f5676ae0017
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-18 21:24:59 +1000

    new

--- scripts/terraform/resource-group.tf
+++ scripts/terraform/resource-group.tf
@@ -5,8 +5,4 @@ resource "azurerm_resource_group" "priya" {
   name     = "priya"
   location = "eastus"
 }
-resource "azurerm_role_assignment" "role_assignment" {
-  principal_id   = "6f8927b1-3336-4d58-8537-dded9616f4f5"
-  role_definition_name = "Owner"
-  scope          = "/subscriptions/6241e51c-c2f5-4ed1-98c5-16824bbdeafa/resourceGroups/priya"
-}
+
