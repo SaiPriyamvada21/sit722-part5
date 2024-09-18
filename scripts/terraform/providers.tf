c28a982976ef38bcffb975964325acd3e8084e4e
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-18 15:01:10 +1000

    new3

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
+      version = "~> 4.2.0"
     }
   }
-
-  required_version = ">= 1.4.0"
 }
+
 
 provider "azurerm" {
   features {}
