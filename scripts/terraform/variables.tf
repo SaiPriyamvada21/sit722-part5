66b78556c8262bc611a0700754c4424a2e306390
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-18 15:55:59 +1000

    new3

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -13,14 +13,3 @@ variable "subscription_id" {
   type = string
 }
 
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
