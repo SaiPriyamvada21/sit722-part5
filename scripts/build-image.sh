cab187c5fd6cdb56510dfdd1ef99ef858a233d8c
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-18 23:37:05 +1000

    new changes2

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,7 +1,1 @@
-set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
-: "$NAME"
-: "$DIRECTORY"
-
-docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ./$DIRECTORY/Dockerfile ./$DIRECTORY
+docker-compose build
