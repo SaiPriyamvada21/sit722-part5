d206d2f281a7eff89b01ffe504b2614d8d351cfb
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-19 14:32:36 +1000

    new7

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,9 +1,16 @@
-set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
-: "$REGISTRY_UN"
-: "$REGISTRY_PW"
-: "$NAME"
+#!/bin/bash
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+# Print the parameters
+echo "Pushing Docker image with the following parameters:"
+echo "CONTAINER_REGISTRY: $CONTAINER_REGISTRY"
+echo "VERSION: $VERSION"
+echo "NAME: $NAME"
+
+# Log in to the container registry
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY -u $REGISTRY_UN --password-stdin
+
+# Tag the Docker image
+docker tag $NAME:$VERSION $CONTAINER_REGISTRY/$NAME:$VERSION
+
+# Push the Docker image
 docker push $CONTAINER_REGISTRY/$NAME:$VERSION
