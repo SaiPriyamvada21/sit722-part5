fc1a2b1fde925c20d83841fbba220255cd80c364
SaiPriyamvada21 <saipriyamvada2112@gmail.com>
2024-09-19 14:12:22 +1000

    new changes

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,23 +1,18 @@
-#
-# Builds a Docker image.
-#
-# Environment variables:
-#
-#   CONTAINER_REGISTRY - The hostname of your container registry.
-#   VERSION - The version number to tag the images with.
-#   NAME - The name of the image to build.
-#   DIRECTORY - The directory form which to build the image.
-#
-# Usage:
-#
-#       ./scripts/build-image.sh
-#
+#!/bin/bash
 
+# Ensure required environment variables are set
 set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
-: "$NAME"
-: "$DIRECTORY"
+: "${CONTAINER_REGISTRY:?CONTAINER_REGISTRY is not set}"
+: "${VERSION:?VERSION is not set}"
+: "${NAME:?NAME is not set}"
+: "${DIRECTORY:?DIRECTORY is not set}"
 
-docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ./$DIRECTORY/Dockerfile ./$DIRECTORY
-# docker build -t "${CONTAINER_REGISTRY}/${NAME}:${VERSION}" --file ./${DIRECTORY}/Dockerfile ./${DIRECTORY}
+# Debug information
+echo "Building Docker image with the following parameters:"
+echo "CONTAINER_REGISTRY: $CONTAINER_REGISTRY"
+echo "VERSION: $VERSION"
+echo "NAME: $NAME"
+echo "DIRECTORY: $DIRECTORY"
+
+# Build the Docker image
+docker buildx -t "${CONTAINER_REGISTRY}/${NAME}:${VERSION}" --file ./${DIRECTORY}/Dockerfile ./${DIRECTORY}
